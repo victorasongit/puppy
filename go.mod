de3d62656617fb3a513590348b49668e5d616cc1
Porcisteanu Victor <victor.porcisteanu@gmail.com>
2025-08-25 12:58:12 +0300

    third

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/victorasongit/puppy
 
 go 1.25.0
+
+require github.com/victorasongit/dog v0.0.0-20250825095224-6d7e2ea5afeb // indirect
--- puppy.go
+++ puppy.go
@@ -1,9 +1,22 @@
 package puppy
 
+import (
+	"github.com/victorasongit/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrownUp(Bark())
+}
+
+
+func BigBarks() string {
+	return dog.WhenGrownUp(Barks())
 }
